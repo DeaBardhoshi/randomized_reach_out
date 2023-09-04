2a51af442513fc060acb8b69dbaa4045af5a7b93
Dea Bardhoshi <62805464+DeaBardhoshi@users.noreply.github.com>
2023-09-04 16:21:46 -0400

    Update randomized_script.py

--- randomized_script.py
+++ randomized_script.py
@@ -13,7 +13,7 @@ import pandas as pd
 client_secret_json = os.environ['CLIENT_SECRET_JSON']
 token_json = os.environ['TOKEN_JSON']
 
-gc = pygsheets.authorize(service_account_env_var=client_secret_json)
+gc = pygsheets.authorize(service_account_env_var='CLIENT_SECRET_JSON')
 
 
 sh = gc.open('Randomized Reach Out')
